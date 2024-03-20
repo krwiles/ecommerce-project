d2cfe08e5529111c163e0b4819d1c40b433fa02f
Kurtis Wiles <kurtis.r.wiles@gmail.com>
2024-03-19 20:32:56 -0400

    Bugfix ProductCateogryRepository

--- 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/dao/ProductCateogryRepository.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/dao/ProductCateogryRepository.java
@@ -1,9 +1,9 @@
 package com.luv2code.ecommerce.dao;
 
-import com.luv2code.ecommerce.entity.Product;
+import com.luv2code.ecommerce.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
-public interface ProductCateogryRepository extends JpaRepository<Product, Long> {
+public interface ProductCateogryRepository extends JpaRepository<ProductCategory, Long> {
 }
