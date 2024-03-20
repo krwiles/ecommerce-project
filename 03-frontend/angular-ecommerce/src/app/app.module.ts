3b72be255afcb90444c5c078fc0ef558a3b7d7b9
Kurtis Wiles <kurtis.r.wiles@gmail.com>
2024-03-20 13:07:15 -0400

    Creates angular product service

--- 03-frontend/angular-ecommerce/src/app/app.module.ts
+++ 03-frontend/angular-ecommerce/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
