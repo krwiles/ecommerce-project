95b2ca0c7244521eba9377231b02da002fb16486
Kurtis Wiles <kurtis.r.wiles@gmail.com>
2024-03-22 20:48:39 -0400

    Refactors cart service

--- 03-frontend/angular-ecommerce/src/app/services/cart.service.ts
+++ 03-frontend/angular-ecommerce/src/app/services/cart.service.ts
@@ -22,12 +22,7 @@ export class CartService {
     if (this.cartItems.length > 0) {
       // find the item in the cart
 
-      for (let tempCartItem of this.cartItems) {
-        if (tempCartItem.id == theCartItem.id) {
-          existingCartItem = tempCartItem;
-          break;
-        }
-      }
+      existingCartItem = this.cartItems.find( tempCartItem => tempCartItem.id === theCartItem.id )!;
 
       // check if we found it
       alreadyExistsInCar = (existingCartItem != undefined);
