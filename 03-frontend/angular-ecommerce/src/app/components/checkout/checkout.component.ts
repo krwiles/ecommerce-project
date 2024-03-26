9c0c7f9cf2470a3cea0ea9e083966ce9edbe9d92
Kurtis Wiles <kurtis.r.wiles@gmail.com>
2024-03-26 14:24:04 -0400

    Implements front end checkout to REST api

--- 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -5,6 +5,11 @@ import {Country} from "../../common/country";
 import {State} from "../../common/state";
 import {Luv2ShopValidators} from "../../validators/luv2-shop-validators";
 import {CartService} from "../../services/cart.service";
+import {CheckoutService} from "../../services/checkout.service";
+import {Router} from "@angular/router";
+import {Order} from "../../common/order";
+import {OrderItem} from "../../common/order-item";
+import {Purchase} from "../../common/purchase";
 
 @Component({
   selector: 'app-checkout',
@@ -28,7 +33,9 @@ export class CheckoutComponent implements OnInit {
 
   constructor(private formBuilder: FormBuilder,
               private luv2ShopFormService: Luv2ShopFormService,
-              private cartService: CartService) { }
+              private cartService: CartService,
+              private checkoutService: CheckoutService,
+              private router: Router) { }
 
   ngOnInit(): void {
 
@@ -148,12 +155,54 @@ export class CheckoutComponent implements OnInit {
 
     if (this.checkoutFormGroup.invalid) {
       this.checkoutFormGroup.markAllAsTouched();
+      return;
     }
 
-    console.log(this.checkoutFormGroup.get('customer')?.value);
-    console.log("The shipping address country is " + this.checkoutFormGroup.get("shippingAddress")?.value.country.name);
-    console.log("The shipping address state is " + this.checkoutFormGroup.get("shippingAddress")?.value.state.name);
+    // set up order
+    let order = new Order();
+    order.totalPrice = this.totalPrice;
+    order.totalQuantity = this.totalQuantity;
 
+    // get cart items
+    const cartItems = this.cartService.cartItems;
+
+    // create orderItems from cartItems
+    let orderItems: OrderItem[] = cartItems.map(cartItem => new OrderItem(cartItem));
+
+    // set up purchase
+    let purchase = new Purchase();
+
+    // populate purchase - customer
+    purchase.customer = this.checkoutFormGroup.controls['customer'].value;
+
+    // populate purchase - addresses
+    purchase.shippingAddress = this.checkoutFormGroup.controls['shippingAddress'].value;
+    const shippingState: State = JSON.parse(JSON.stringify(purchase.shippingAddress.state));
+    const shippingCountry: Country = JSON.parse(JSON.stringify(purchase.shippingAddress.country));
+    purchase.shippingAddress.state = shippingState.name;
+    purchase.shippingAddress.country = shippingCountry.name;
+
+    purchase.billingAddress = this.checkoutFormGroup.controls['billingAddress'].value;
+    const billingState: State = JSON.parse(JSON.stringify(purchase.billingAddress.state));
+    const billingCountry: Country = JSON.parse(JSON.stringify(purchase.billingAddress.country));
+    purchase.billingAddress.state = billingState.name;
+    purchase.billingAddress.country = billingCountry.name;
+
+    // populate purchase - order and orderItems
+    purchase.orderItems = orderItems;
+    purchase.order = order;
+
+    // call REST api via CheckoutService
+    this.checkoutService.placeOrder(purchase).subscribe({
+        next: response => {
+          alert(`Your order has been recieved.\nOrder tracking number: ${response.orderTrackingNumber}`);
+          this.resetCart();
+        },
+        error: err => {
+          alert(`There was an error: ${err.message}`)
+        }
+      }
+    );
   }
 
   copyShippingAddressToBillingAddress(event: Event) {
@@ -226,5 +275,21 @@ export class CheckoutComponent implements OnInit {
     this.cartService.totalPrice.subscribe(
       totalPrice => this.totalPrice = totalPrice
     );
+  }
+
+  private resetCart() {
+    // reset cart data
+    this.cartService.cartItems = []
+    this.cartService.totalPrice.next(0);
+    this.cartService.totalQuantity.next(0);
+    console.log("resetCart has reset cartservice")
+
+    // reset form data
+    this.checkoutFormGroup.reset();
+    console.log("resetCart has run checkout form rest")
+
+    // navigate back to product page
+    this.router.navigateByUrl("/products");
+    console.log("resetCart has naviage")
   }
 }
--- 03-frontend/angular-ecommerce/src/app/validators/luv2-shop-validators.ts
+++ 03-frontend/angular-ecommerce/src/app/validators/luv2-shop-validators.ts
@@ -6,9 +6,15 @@ export class Luv2ShopValidators {
   static notOnlyWhitespace(control: FormControl): ValidationErrors | null {
 
     // check if string only contains whitespace
-    if ((control.valid != null) && (control.value.trim().length === 0)) {
-      // invalid, return error object
-      return { 'notOnlyWhitespace': true };
+    if (control.value != null) {
+      if (control.value.trim().length === 0) { // when using && it still checks both and will cause an error trying to .trim() a null value
+        // invalid, return error object
+        return { 'notOnlyWhitespace': true };
+      }
+      else {
+        // valid, return null
+        return null;
+      }
     }
     else {
       // valid, return null
